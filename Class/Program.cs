6209b9d52ba97b009c4775d24adac0e82777fb63
admin <admin@CHETUIWK1240>
2022-03-15 15:23:11 +0530

    Add project files.

---
+++ Class/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        public void get(out int i)
+        {
+           
+            var random = new Random();
+            i = random.Next();
+           
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            int random1;
+            obj.get(out random1);
+            Console.WriteLine(random1);
+
+        }
+    }
+}
